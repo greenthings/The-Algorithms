e30d7ee155575c02a11c8e114ef662535359c0af
Jun Shin <mnhfn8hj@gmail.com>
2024-02-18 18:16:24 +0900

    [EDIT] BubbleSort Algorithm

--- c++/sorting_algorithms/BubbleSort.cpp
+++ c++/sorting_algorithms/BubbleSort.cpp
@@ -1,32 +1,30 @@
 #include "BubbleSort.h"
 #include "../common/common.h"
 
-// Time Complexity in Worst case: O(N^2) 
+// Time Complexity in Worst case: O(N^2)
 
 // using nested loop, such as two for statements
 void BubbleSort_using_nested_loop(int arr[]) {
   for (int i = 0; arr[i] != '\0'; i++) {
-    for (int j = i + 1; arr[j] != '\0'; j++) {
-      if (arr[i] > arr[j]) {
-        swap(arr[i], arr[j]);
+    for (int j = 0; arr[j] != '\0'; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
       }
     }
   }
 }
 
-
 // using do while
-void BubbleSort_using_do_while(int arr[]) { 
-    bool sorted = true;
-    
-    do{
-        sorted = true;
-        for (int i = 0; arr[i+1] != '\0'; i++){
-            if(arr[i] > arr[i+1]){
-                swap(arr[i], arr[i+1]);
-                sorted = false;
-            }
-        }
-    }while(sorted != true);
-    
+void BubbleSort_using_do_while(int arr[]) {
+  bool sorted = true;
+
+  do {
+    sorted = true;
+    for (int i = 0; arr[i + 1] != '\0'; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr[i], arr[i + 1]);
+        sorted = false;
+      }
+    }
+  } while (sorted != true);
 }
